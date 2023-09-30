7525eb7560e927662a401cedd77a0be582f760ca
petrkopecky <petr.kopecky@gmail.com>
2023-09-30 12:15:42 +0200

    product add

--- products-fe/src/components/ProductAdd.tsx
+++ products-fe/src/components/ProductAdd.tsx
@@ -4,12 +4,28 @@ interface FormProps {
 }
 
 export default function ProductAdd({ onProductAdd }: FormProps) {
+  function onProductAddClick() {
+    fetch("/api/products", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "Mobilni telefon",
+        price: 14999.0,
+        description: "skvely novy telefon",
+      }),
+    });
+    onProductAdd();
+  }
+
   return (
     <>
       <p>
         <b>new product</b>
       </p>
-      <button onClick={onProductAdd} className="button">
+      <button onClick={onProductAddClick} className="button">
         add
       </button>
       <button onClick={onProductAdd} className="button">
